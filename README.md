624fa030eef45053c2b6656c66856bf95826824b
Reuel Freitas Cunha <reuel.cf@outlook.com>
2021-03-26 14:02:28 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# taskapp
+# TasksApp
---
+++ nova_tarefa.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Tasks App</title>
+
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+	</head>
+
+	<body>
+		<nav class="navbar navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src="img/logo2.png" width="35" height="35" class="d-inline-block align-top" alt="">
+					Tasks App
+				</a>
+			</div>
+		</nav>
+
+		<? if( isset($_GET['inclusao']) && $_GET['inclusao'] == 1 ) { ?>
+			<div class="bg-success pt-2 text-white d-flex justify-content-center">
+				<h5>Tarefa inserida com sucesso!</h5>
+			</div>
+		<? } ?>
+
+		<div class="container app">
+			<div class="row">
+				<div class="col-md-3 menu">
+					<ul class="list-group">
+						<li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
+						<li class="list-group-item active"><a href="#">Nova tarefa</a></li>
+						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-9">
+					<div class="container pagina">
+						<div class="row">
+							<div class="col">
+								<h4>Nova tarefa</h4>
+								<hr />
+
+								<form method="post" action="tarefa_controller.php?acao=inserir">
+									<div class="form-group">
+										<label>Descrição da tarefa:</label>
+										<input type="text" class="form-control" name="tarefa" placeholder="Exemplo: Lavar o carro">
+									</div>
+
+									<button class="btn btn-success">Cadastrar</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
